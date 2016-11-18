8e65210ddae1e556167e27f5cc0aafda9b5411aa
TMLBNSF <srikanthvrns@gmail.com>
2016-11-17 18:59:20 -0600

    Update all fields using Xpath and Css selectors

--- TestSelenium/src/testselenium/Myclass.java
+++ TestSelenium/src/testselenium/Myclass.java
@@ -1,12 +1,25 @@
 package testselenium;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.interactions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.sun.jna.platform.mac.Carbon.EventHotKeyID.ByValue;
 
@@ -14,22 +27,44 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Myclass {
 
- public static void main(String[] args) {
-	 //System.setProperty("webdriver.firefox.marionette","/Users/srikanthvrns/Documents/geckodriver");
-	 System.setProperty("webdriver.chrome.driver", "/Users/srikanthvrns/Documents/chromedriver"); 
-	 WebDriver driver = new ChromeDriver();
-  
-	 driver.get("https://goo.gl/forms/DXXzjuPvi695hZGg1");
-	 
-	 driver.manage().window().maximize();
-  
+ @SuppressWarnings("deprecation")
+ @Test
+public void testChrome() throws InterruptedException {
 
-	 driver.findElement(By.name("entry.1456069782")).sendKeys("HareRam"); //username	
-	 driver.findElement(By.name("entry.1565420787")).sendKeys("HareRam@yomail.com"); //email
-	 driver.findElement(By.name("entry.1841232065")).sendKeys("hariom"); //phone Number
+	//System.setProperty("webdriver.gecko.driver", "/Users/srikanthvrns/Documents/geckodriver");
+	//MarionetteDriver driver = new MarionetteDriver();
+
+	System.setProperty("webdriver.chrome.driver", "/Users/srikanthvrns/Documents/chromedriver"); 
+	WebDriver driver = new ChromeDriver();
+  
+	driver.get("https://goo.gl/forms/DXXzjuPvi695hZGg1");
+	 
+	driver.manage().window().maximize();
+	
+	 //1st page
+
+	 driver.findElement(By.name("entry.1456069782")).sendKeys("Tarun"); //username	
+	 driver.findElement(By.name("entry.1565420787")).sendKeys("Elon@yomail.com"); //email
+	
+	 //Date
+	 driver.findElement(By.cssSelector("input[type='date']")).sendKeys("09252017");
+	 
+	 //Password
+	 driver.findElement(By.name("entry.1841232065")).sendKeys("9869449292"); 
+	 
+	 driver.findElement(By.cssSelector("input[aria-label='firstname']")).sendKeys("jagdish");
+	 driver.findElement(By.cssSelector("input[aria-label='lastname']")).sendKeys("wankde");
+	 
+	 //address
+	 driver.findElement(By.name("entry.1254820097")).sendKeys("Plano Dallas TX");
+	 
+	 //phone number
+	 driver.findElement(By.name("entry.1506862664")).sendKeys("9120450189");
 	
 	 driver.findElement(By.className("freebirdFormviewerViewNavigationButtons")).click(); //next button on first page
 	
+	 // 2nd Page
+	 
 	 Set<String> set1=driver.getWindowHandles();
 	 Iterator<String> win1=set1.iterator();
 	
@@ -37,10 +72,57 @@ public class Myclass {
 	
 	 driver.switchTo().window(child);
 	 
-	 driver.findElement(By.name("entry.2084856316")).sendKeys("1234567890");
-	 driver.findElement(By.name("entry.1339122660")).click();
-	 driver.findElement(By.cssSelector("div[jsname='MPu53c']")).click();
+	//Comments
+	 driver.findElement(By.cssSelector("textarea[jsname='YPqjbf']")).sendKeys("Please enter some Text");
+	  
+	 //Income
+	 driver.findElement(By.cssSelector("div[aria-label='$60000-$80000']")).click();
 	 
+	 //DropDown Fix Using Xpath
+	 driver.findElement(By.xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[1]/div[1]")).click();
+	 driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+	 driver.findElement(By.xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[3]/div[2]/div[2]/div[6]")).click();
+	 Thread.sleep(500);
+	 
+	 //Age
+	 driver.findElement(By.cssSelector("div[aria-label='18-25']")).click();
+	 
+	 //Fav color
+	 driver.findElement(By.cssSelector("div[aria-label='Blue']")).click();
+     
+	 //Rating
+	 driver.findElement(By.cssSelector("div[ aria-posinset='7']")).click();
+     
+	 //Time in hours
+	 driver.findElement(By.xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[8]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input")).sendKeys("12");
+     
+	 //Time in minutes
+	 driver.findElement(By.xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[8]/div[2]/div[3]/div/div[1]/div/div[1]/input")).sendKeys("30");
+
+	 //Next Button
+     driver.findElement(By.cssSelector("div[jsname='OCpkoe']")).click();
+     
+     //3rd Page
+     
+     String parentWindowHandler1 = driver.getWindowHandle(); 
+	 Set<String> set3=driver.getWindowHandles();
+	 Iterator<String> win3=set3.iterator();
+	
+	 String child3=win3.next();
+	
+	 driver.switchTo().window(child3);
+	 
+	 //Credit Card Number
+	 driver.findElement(By.name("entry.216505432")).sendKeys("72775439103456");
+	 
+	 //Credit Card Name
+	 driver.findElement(By.name("entry.952063220")).sendKeys("Jagdish Wankde");
+	 
+	 //Date
+	 driver.findElement(By.cssSelector("input[type='date']")).sendKeys("09252017");
+	 
+	 //Submit
+	 driver.findElement(By.cssSelector("div[jsname='M2UYVd']")).submit();
 	 
  }
 }
